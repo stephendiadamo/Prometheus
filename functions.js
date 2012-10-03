d910c5e3efcb94ed66e833deb8e41e275d83ef03
darktemplar <danielthian2010@gmail.com>
2012-10-03 13:49:05 -0400

    Changes to Object Classes

--- functions.js
+++ functions.js
@@ -12,132 +12,154 @@ var currentSelectedShape;
 var currentOutlineColor = "#000";
 var currentfillColor = "#000"
 
+var currentSelectedShape = null;
+var currentDraw = null;
+var shapesTable = [];
+
+// Do not OVERRIDE these variables!
+var canvas;
+var context;
 // Objects
 
-// Can use a Shape parent class possibly
-// function Shape (x, y, outlineColor, outlineThickness, fillColor) {
-// this.x = x;
-// this.y = y;
-// this.outlineColor = outlineColor;
-// this.outlineThickness = outlineThickness;
-// this.fillColor = fillColor;
-// }
+// Initialization 
+$(document).ready(function() {
+    // Set up variables neccessary for drawing
+    canvas = document.getElementById("drawingCanvas");
+    context = canvas.getContext('2d');
+});
 
-function Circle() {
-    this.x = 0;
-    this.y = 0;
-    this.radius = 1;
+    /*
+     * Points define the type of shape
+     */
+function DefaultProperty(){
+    var x, y;
+    
+    this.points = []; // Can be clicked.
+    this.arcs = [];
+    
     this.fillColor = "#000"
-    this.outlineColor = "#000";
-    this.outlineThickness = 1;
-}
-
-function Line() {
-    this.x1 = 0;
-    this.y1 = 0;
-    this.x2 = 1;
-    this.y2 = 1;
-    this.lineColor = "#000"
+    this.lineColor = "#000";
     this.lineThickness = 1;
+    this.scaleX = 1;
+    this.scaleY = 1;
+    this.Rotate = 0.0;
+    
+    this.translate(newX, newY){
+        x = newX;
+        y = newY;
+    }
 }
 
-function Rectangle() {
-    this.x = 0;
-    this.y = 0;
-    this.l = 1;
-    this.w = 1;
-    this.fillColor = "#000"
-    this.outlineColor = "#000";
-    this.outlineThickness = 1;
+/*
+ * Assume all shapes move Clockwise
+ * Shapes are created by click and dragging.
+ */
+function Line(pointStart){
+    DefaultProperty.call(this);
+    this.x = pointStart[0];
+    this.y = pointStart[1];
+    
+    var x_end = 0;
+    var y_end = 0;
+    
+    this.setDraw = function(secondPoint){
+        x_end = secondPoint[0]
+        y_end = secondPoint[1]
+    }
+    this.draw = function(){
+        context.beginPath();
+        context.moveTo(this.x, this.y);
+        context.scale(this.scaleX,this.scaleY);
+        
+        context.strokeStyle = currentOutlineColor;
+        context.fillStyle = outlineColor;
+        
+        context.lineTo(x_end, y_end);
+        context.closePath();
+    }
 }
 
-// Can be built into constructor, but must be added to the shapes array.
-function addCircle(x, y, radius, fillColor, outlineColor, outlineThickness) {
-    var circle = new Circle;
-    circle.x = x;
-    circle.y = y;
-    circle.radius = radius;
-    circle.fillColor = fillColor;
-    circle.outlineColor = outlineColor;
-    circle.outlineThickness = outlineThickness;
-    shapes.push(circle);
+function Rectangle(pointStart) {
+    DefaultProperty.call(this);
+    DefaultProperty.x = pointStart[0];
+    DefaultProperty.y = pointStart[1];
+
+    DefaultPropery.fillColor = "#000"
+    DefaultPropery.outlineColor = "#000";
+    DefaultPropery.outlineThickness = 1;
+    
+    var length = 0;
+    var height = 0;
+    
+    this.setDraw = function (secondPoint){
+        length = x - secondPoint[0];
+        height = y - secondPoint[1];
+    }
+    
+    // Defines the unique Shape
+    this.draw = function(){
+        context.moveTo(this.x, this.y);
+        context.scale(this.scaleX,this.scaleY);
+        
+        context.strokeStyle = currentOutlineColor;
+        context.fillStyle = outlineColor;
+        
+        context.rect(0,0,length,height);
+        
+        //DefaultProperty.points[1] = [DefaultProperty.x + length, DefaultProperty.y];
+        //DefaultProperty.points[2] = [DefaultProperty.x + length, DefaultProperty.y + height];
+        //DefaultProperty.points[3] = [DefaultProperty.x, DefaultProperty.y + height];
+    }
 }
 
-function addLine(x1, y1, x2, y2, lineColor, lineThickness) {
-    var line = new Line;
-    line.x1 = x1;
-    line.y1 = y1;
-    line.x2 = x2;
-    line.y2 = y2;
-    line.lineColor = lineColor;
-    line.lineThickness = lineThickeness;
-    shapes.push(line);
+function Circle(pointStart){
+    DefaultProperty.call(this);
+    DefaultProperty.points = null;
+    var radius = pointStart - pointEnd; // Radius
+    
+    this.setDraw = function(secondPoint){
+        radius = pointStart - pointEnd;
+    }
+    this.draw = function(){
+        context.moveTo(this.x, this.y);
+        context.scale(this.scaleX,this.scaleY);
+        
+        context.strokeStyle = currentOutlineColor;
+        context.fillStyle = outlineColor;
+        
+        context.arc(0, 0, radius, 0, 2 * Math.PI);
+    }
 }
 
-function addRectangle(x, y, l, w, fillColor, outlineColor, outlineThickness) {
-    var rectangle = new Rectangle;
-    rectangle.x = x;
-    rectangle.y = y;
-    rectangle.l = l;
-    rectangle.w = w;
-    rectangle.fillColor = fillColor;
-    rectangle.outlineColor = outlineColor;
-    rectangle.outlineThickness = outlineThickness;
-    shapes.push(rectangle);
-}
-
-function setSelected(shape) {
+function setSelected(event){
     document.getElementById("curShape").innerHTML = "Shape : " + shape;
-    switch (shape) {
-    case RECTANGLE:
-        rectangleSelected = true;
-        circleSelected = false;
-        straightLineSelected = false;
+    currentSelectedShape = shape;
+}
+
+/*
+ * Note: this should be binded to elements with Jquery, for now just use html
+ */
+function commandCanvas(var command) {
+    switch (command){
+    case "clear":
+        context.clearRect(0, 0, canvas.width, canvas.height);
         break;
-    case CIRCLE:
-        rectangleSelected = false;
-        circleSelected = true;
-        straightLineSelected = false;
+    case "save":
+        var dataURL = canvas.toDataURL();
+        alert("THIS IS THE ENCODED PNG: \n" + dataURL);
         break;
-    case LINE:
-        rectangleSelected = false;
-        circleSelected = false;
-        straightLineSelected = true;
-        break;
-    default:
+    case "undo":
         break;
     }
 }
 
-function clearCanvas() {
-    var canvas = document.getElementById("drawingCanvas");
-    var context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-function circleClicked() {
-    setSelected(CIRCLE);
-}
-
-function straightLineClicked() {
-    setSelected(LINE);
-}
-
-function rectangleClicked() {
-    setSelected(RECTANGLE);
-}
-
-function saveImage() {
-    var canvas = document.getElementById("drawingCanvas");
-    var context = canvas.getContext('2d');
-    var dataURL = canvas.toDataURL();
-    alert("THIS IS THE ENCODED PNG: \n" + dataURL);
-}
-
 // Get the mouse coordinates with respect to the canvas
 function updateMouseCoordinates(event) {
+    // Paranoid check
+    if (event == null && canvas == null)
+        return null;
+    
     var coordinates = document.getElementById("coords");
-    var canvas = document.getElementById("drawingCanvas");
 
     // With respect to the canvas
     var x = event.pageX - canvas.offsetLeft;
@@ -149,40 +171,45 @@ function updateMouseCoordinates(event) {
     return [ x, y ];
 }
 
-function drawShape(event) {
-    var canvas = document.getElementById("drawingCanvas");
-    var context = canvas.getContext('2d');
-
-    var coordinates = updateMouseCoordinates(event);
-    var x = coordinates[0];
-    var y = coordinates[1];
-
-    if (rectangleSelected) {
-        // Rectangle logic here
-        context.beginPath();
-        context.moveTo(x, y);
-        context.lineTo(x, y + 50);
-        context.lineTo(x + 50, y + 50);
-        context.lineTo(x + 50, y);
-
-    } else if (circleSelected) {
-        // Circle logic here
-        context.beginPath();
-        context.arc(x, y, 40, 0, 2 * Math.PI);
-
-    } else if (straightLineSelected) {
-        // Line logic here
-        context.beginPath();
-        context.moveTo(x, y);
-        context.lineTo(x + 50, y + 50);
-    }
-    context.closePath();
-    context.strokeStyle = currentOutlineColor;
-    context.stroke()
+function finishShape(event){
+    
 }
 
-function undoLast() {
-    return;
+// Fired off until click RELEASE event
+function modifyShape(event){
+    var coordinates = updateMouseCoordinates(event);
+    
+    renderShapes(drawShape);
+}
+
+// Fired off on first click HOLD event
+function addShape(event){
+    var coordinates = updateMouseCoordinates(event);
+    
+    // First we add the shape
+    var drawShape = null;
+    switch (currentSelectedShape){
+        case RECTANGLE:
+            drawShape = new Rectangle(coordinates);
+            break;
+        case CIRCLE:
+            drawShape = new Circle(coordinates);
+            break;
+        case LINE:
+            drawShape = new Line(coordinates);
+            break;
+    }
+    if (drawShape!= null){
+        shapesTable.push(drawShape);
+        currentDraw = drawShape;
+    }
+    renderShapes(drawShape);
+}
+
+function renderShapes(shape){
+    for (var i = 0; i < shapesTable.length; i ++){
+        shapesTable[i].draw();
+    }
 }
 
 function setColor(color) {
