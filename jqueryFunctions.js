d2fe7e9de91075e510ae29d9e6c5b8193c562905
stephendiadamo <stephen.diadamo@gmail.com>
2012-10-05 18:01:55 -0400

    SD - Modified logic for animation

--- jqueryFunctions.js
+++ jqueryFunctions.js
@@ -4,45 +4,65 @@ var straightLinePalletShowing = false;
 var circlePalletShowing = false;
 
 $(document).ready(function() {
-    $("#rectangle").click(function() {
-    	if (!rectanglePalletShowing){
-        	$("#rectanglePallet").html($("#colorPallet").html());
-        	$("#rectanglePallet").slideDown(600);
-        	rectanglePalletShowing = true;
-        } else {
+
+
+	function hidePallets(rect, circle, line){
+		if (rect){
         	$("#rectanglePallet").slideUp(600, function() {
         		$("#rectanglePallet").html("");
         		$("#rectanglePallet").css("display", "none");
         	});
         	rectanglePalletShowing = false;
+		}
+
+		if (circle){
+			$("#circlePallet").slideUp(600, function() {
+        		$("#circlePallet").html("");
+        		$("#circlePallet").css("display", "none");
+        	});
+        	circlePalletShowing = false;
+		}
+
+		if (line){
+			$("#straightLinePallet").slideUp(600, function() {
+        		$("#straightLinePallet").html("");
+        		$("#straightLinePallet").css("display", "none");
+        	});
+        	straightLinePalletShowing = false;
+		}
+	}
+
+    $("#rectangle").click(function() {
+    	if (!rectanglePalletShowing){
+    		hidePallets(false, true, true);
+        	$("#rectanglePallet").html($("#colorPallet").html());
+        	$("#rectanglePallet").slideDown(600);
+        	rectanglePalletShowing = true;
+        } else {
+			hidePallets(true, false, false);
         }
     });
 
 	$("#straightLine").click(function() {
     	if (!straightLinePalletShowing){
+    		hidePallets(true, true, false);
+
         	$("#straightLinePallet").html($("#colorPallet").html());
         	$("#straightLinePallet").slideDown(600);
         	straightLinePalletShowing = true;
         } else {
-        	$("#straightLinePallet").slideUp(600, function() {
-        		$("#straightLinePallet").html("");
-        		$("#straightLinePallet").css("display", "none");
-        	});
-        	straightLinePalletShowing = false;
+        	hidePallets(false, false, true);
         }
     });
 
     $("#circle").click(function() {
     	if (!circlePalletShowing){
+    		hidePallets(true, false, true);
         	$("#circlePallet").html($("#colorPallet").html());
         	$("#circlePallet").slideDown(600);
         	circlePalletShowing = true;
         } else {
-        	$("#circlePallet").slideUp(600, function() {
-        		$("#circlePallet").html("");
-        		$("#circlePallet").css("display", "none");
-        	});
-        	circlePalletShowing = false;
+			hidePallets(false, true, false);
         }
     });
 });
